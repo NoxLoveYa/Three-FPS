03aa5fb931f45a98115746f90c4abbae7db3e764
NoxLoveYa <ewan.massonet@epitech.eu>
2024-05-16 18:07:42 +0200

    [UPDATED] mesh array && material array && [UPDATED] scene now contains two cubes

--- app.js
+++ app.js
@@ -10,11 +10,38 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//Create all the objects
+const geometry = {
+	"cube": new THREE.BoxGeometry( 1, 1, 1 ),
+	"plane": new THREE.PlaneGeometry( 5, 5 ),
+	"sphere": new THREE.SphereGeometry( 1, 32, 32 ),
+	"torus": new THREE.TorusGeometry( 1, 0.4, 16, 100 )
+};
+
+const material = {
+	"basic": new THREE.MeshBasicMaterial( { color: "rgb(255, 0, 0)" } ),
+	"lambert": new THREE.MeshLambertMaterial( { color: "rgb(255, 0, 0)" } ),
+	"phong": new THREE.MeshPhongMaterial( { color: "rgb(255, 0, 0)" } ),
+	"standard": new THREE.MeshStandardMaterial( { color: "rgb(255, 0, 0)" } ),
+	"toon": new THREE.MeshToonMaterial( { color: "rgb(255, 0, 0)" } )
+};
+
 //Create cube and add to scene
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial( { color: "rgb(255, 0, 0)" } );
-const cube = new THREE.Mesh( geometry, material );
+const cube = new THREE.Mesh( geometry.cube.clone(), material.phong.clone() );
+cube.position.setX(1);
+cube.position.setY(1);
+cube.material.color.set( "rgb(75, 255, 0)" );
 scene.add( cube );
+
+const cube2 = new THREE.Mesh( geometry.cube.clone(), material.phong.clone() );
+cube2.position.setX(-1);
+cube2.position.setY(1);
+cube2.material.color.set( "rgb(148, 0, 211)"  );
+scene.add( cube2 );
+
+//Create grid and add to scene
+const gridHelper = new THREE.GridHelper( 5, 5 );
+scene.add( gridHelper );
 
 //Create light and add to scene
 const ambientLight = new THREE.AmbientLight( "rgb(255, 255? 255)", 0.1 );
@@ -25,9 +52,9 @@ directionalLight.position.set( 1, 1, 1 ).normalize();
 scene.add( directionalLight );
 scene.add( ambientLight );
 
-
 //Setup scene and camera
 camera.position.z = 5;
+camera.position.y = 2;
 
 //Add orbit controls
 const controls = new OrbitControls( camera, renderer.domElement );
@@ -39,6 +66,9 @@ const animate = function () {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
+	cube2.rotation.x -= 0.01;
+	cube2.rotation.y -= 0.01;
 
 	controls.update();
 
