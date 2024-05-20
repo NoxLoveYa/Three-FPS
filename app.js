d35f616f6d7cf9660f2443b63904488e71ba0288
NoxLoveYa <ewan.massonet@epitech.eu>
2024-05-20 10:11:20 +0200

    [ADD] auto resize

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ scene.add( cube );
 //Setup scene and camera
 camera.position.z = 5;
 
+//resize renderer on window resize
+window.addEventListener( 'resize', function () {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}, false );
+
 //Animate scene
 const animate = function () {
 	requestAnimationFrame( animate );
